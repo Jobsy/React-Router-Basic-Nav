74d8be3975fa03c76515e420ce0e60a0083b3e84
Oluwajoba <oluwajobabello@gmail.com>
2019-09-16 20:08:03 +0100

    create <Route /> for the Nav items

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
+
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
+
+
 );
 
 export default App;
